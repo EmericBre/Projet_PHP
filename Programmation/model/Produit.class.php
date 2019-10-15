de76e78770465ec61692dd6bb9ae42d925ca9c76
BIHAN Baptiste <bihanb@hostname.iut2.upmf-grenoble.fr>
2019-10-15 11:12:47 +0200

    début du model

---
+++ Programmation/model/Produit.class.php
@@ -0,0 +1,38 @@
+<?php
+
+  class Article {
+    private $ref; //référence unique du produit
+    private $image; //image du produit
+    private $titre; //nom du produit
+    private $description; //texte descriptif
+    private $caracteristiques; //caractéristiques
+    private $prix; //prix du produit
+
+    //getters
+    function getRef() : int {
+        return $this->ref;
+    }
+
+    function getImage() : string {
+        return $this->image;
+    }
+
+    function getTitre() : string {
+        return $this->titre;
+    }
+
+    function getDescription() : string {
+        return $this->description;
+    }
+
+    function getCaractéristiques() : string {
+        return $this->caracteristiques;
+    }
+
+    function getPrix() : float {
+        return $this->prix;
+    }
+
+  }
+
+ ?>
