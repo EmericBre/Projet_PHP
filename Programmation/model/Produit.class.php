346147866d66a02cb5e7b49dbae5f81e2036f773
BRETON Emeric <bretonem@hostname.iut2.upmf-grenoble.fr>
2019-10-17 11:26:13 +0200

    Création des classes et construction

--- Programmation/model/Produit.class.php
+++ Programmation/model/Produit.class.php
@@ -1,6 +1,6 @@
 <?php
 
-  class Article {
+  class Produit {
     private $ref; //référence unique du produit
     private $image; //image du produit
     private $titre; //nom du produit
@@ -31,6 +31,27 @@
 
     function getPrix() : float {
         return $this->prix;
+
+    }
+
+    // Le constructeur peut être appelé par la Base de Données (BD)
+    function __construct(string $ref=NULL,string $titre=NULL,float $prix=NULL) {
+
+      // Les autres paramètres ont une valeur nulle, et il ne faut pas modifier les valeurs données par la BD
+      if (isset($ref)) {
+        // Si ref est renseignés alors toutes les autres valeurs doivent l'être
+        $this->ref = $ref;
+        $this->titre = $titre;
+        $this->prix = $prix;
+      }
+
+      // Ces valeurs doivent être renseignées
+      assert(isset($this->ref));
+      assert(isset($this->titre));
+      assert(isset($this->prix));
+
+      // Ces valeurs doivent être strictement positives
+      assert($this->prix > 0);
 
     }
 
