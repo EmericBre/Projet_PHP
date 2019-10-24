2e362d85149c218d310d3382bd59c4723d53f7a2
BIHAN Baptiste <bihanb@hostname.iut2.upmf-grenoble.fr>
2019-10-24 10:40:05 +0200

    b

--- Programmation/view/articles.view.php
+++ Programmation/view/articles.view.php
@@ -1,40 +1,14 @@
 <html>
 <head>
   <title>Gun and Co</title>
-  <meta charset="UTF-8"/>
-  <meta http-equiv="content-type" content="text/html;" />
-  <meta name="author" content="Jean-Pierre Chevallet" />
-  <link rel="stylesheet" type="text/css" href="../view/design/style.css"
 </head>
 
 <body>
   <header>
     <h1>Gun and Co, la démocratie à petit prix</h1>
-    <p><img src="../view/design/pub.png"/></p>
   </header>
-  <?php
 
+  
 
-  ///////////////////////////////////////////////////////
-  //   A COMPLETER
-  ///////////////////////////////////////////////
-
-  ?>
-
-  <nav>
-
-  </nav>
-
-  <?php
-  ///////////////////////////////////////////////////////
-  //  A COMPLETER
-  ///////////////////////////////////////////////
-
-  // Affichage des articles
-
-  ?>
-  <footer>
-  </p>Site fictif, issus de données réelles du Web</p>
-</footer>
 </body>
 </html>
