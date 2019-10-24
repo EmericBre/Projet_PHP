d24d082c82269f682539204dbacab938cbf902d8
BRETON Emeric <bretonem@hostname.iut2.upmf-grenoble.fr>
2019-10-24 10:25:33 +0200

    a

--- Programmation/model/DAO.test.getAllProduit.php
+++ Programmation/model/DAO.test.getAllProduit.php
@@ -11,4 +11,5 @@ $cat = $dao->getAllProduit();
 // Affiche 1 produit pour le test
 print($cat[2]->getRef().' '.$cat[2]->getCategorie().' '.$cat[2]->getImage().' '.$cat[2]->getTitre().' '.$cat[2]->getDescription().' '.$cat[2]->getCaractéristiques());
 print($cat[2]->getPrix().' '.$cat[2]->getFabricant().' '.$cat[2]->getDispo().' '."\n");
- ?>
+
+?>
